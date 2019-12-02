d0c3cf5d2d42e4332943f61a8338bb58042c3efc
alexjununo <alex.jpp10@gmail.com>
2019-12-02 08:19:21 -0300

    adjusts

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
 // Método que retorna as notas do arquivo json
 const fecthNotes = () => {
     // UTILIZE O fs PARA LER OS DADOS DO ARQUIVO JSON QUE VOCÊ IRÁ SALVAR AS NOTAS
@@ -7,10 +5,10 @@ const fecthNotes = () => {
 
 // Método que salva as notas no arquivo json
 const saveNotes = (notes) => {
-    // UTILIZA O FS PARA SALVAR OS DADOS NO ARQUIVO JSON
+    // UTILIZA O fs PARA SALVAR OS DADOS NO ARQUIVO JSON
 };
 
-exports.addNote = (title, body) => {
+const addNote = (title, body) => {
     // MÉTODO PARA ADICIONAR NOTAS
     // VALIDAR SE JÁ EXISTE NOTA COM O MESMO TITULO, SE SIM,
     // ENVIAR MENSAGEM AVISANDO QUE JÁ EXISTE, SE NÃO
@@ -18,23 +16,21 @@ exports.addNote = (title, body) => {
     // OBS: Deve validar caso não exista notas
 };
 
-exports.removeNote = (title) => {
+const removeNote = (title) => {
     // REMOVER NOTA DE ACORDO COM O TITULO ENVIADO
 };
 
-exports.getAllNotes = () => {
+const getAllNotes = () => {
     // BUSCAR TODAS AS NOTAS
     // DICA: Método fetchNotes pode ajudar kkkk
 };
 
-exports.getNote = (title) => {
+const getNote = (title) => {
     // BUSCAR NOTA PELO TÍTULO, SE NÃO EXISTIR, INFORMAR QUE NÃO EXISTE KKK
     // SE A NOTA EXISTE, DEVE RETORNÁ-LA
 };
 
-exports.updateNote = (title, body) => {
+const updateNote = (title, body) => {
     // BUSCAR NOTAS PELO TÍTULO E ATUALIZAR O CONTEUDO
     // DEVE RETORNAR A NOTA ATUALIZADA
 };
-
-module.exports = exports;
